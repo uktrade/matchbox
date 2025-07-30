06efc36e6a4ea6a80798c4bcd2743a69fd5ec9a8
Aidan Russell <russell_aidan@hotmail.com>
2025-07-30 21:23:56 +0100

    feature: logging platform related code is separated out as vendor-specific

--- src/matchbox/common/logging.py
+++ src/matchbox/common/logging.py
@@ -2,7 +2,6 @@
 
 import json
 import logging
-import os
 from datetime import datetime, timezone
 from functools import cached_property
 from typing import Any, Final, Literal
@@ -90,12 +89,6 @@ def build_progress_bar(console_: Console | None = None) -> Progress:
 class ASIMFormatter(logging.Formatter):
     """Format logging with ASIM standard fields."""
 
-    _tracer = None
-    """Datadog tracer instance."""
-
-    def _get_first_64_bits_of(self, trace_id):
-        return str((1 << 64) - 1 & trace_id)
-
     @cached_property
     def event_severity(self) -> dict[str, str]:
         """Event severity level lookup."""
@@ -107,64 +100,9 @@ class ASIMFormatter(logging.Formatter):
             "CRITICAL": "High",
         }
 
-    @cached_property
-    def container_id(self) -> str:
-        """AWS ECS container ID.
-
-        This environment variable is injected by AWS to all ECS tasks.
-        """
-        uri = os.environ.get("ECS_CONTAINER_METADATA_URI", "")
-        return uri.split("/")[-1] if uri else ""
-
-    @cached_property
-    def env(self) -> str:
-        """Datadog's environment value.
-
-        This environment variable is set by the ECS task definition.
-        """
-        return os.getenv("DD_ENV", default="")
-
-    @cached_property
-    def service(self) -> str:
-        """Datadog's service value.
-
-        This environment variable is set by the ECS task definition.
-        """
-        return os.getenv("DD_SERVICE", default="")
-
-    @cached_property
-    def version(self) -> str:
-        """Datadog's version value.
-
-        This environment variable is set in the Dockerfile for the task.
-        """
-        return os.getenv("DD_VERSION", default="")
-
-    def get_trace_id_span_id(self) -> tuple[str | None, str | None]:
-        """Retrieve's Datadog's trace ID and span ID variables.
-
-        These two variables are discovered by the Datadog Python tracing library.
-        """
-        # ddtrace is a server-side dependency
-        # imported here as logging.py is in common, and is imported
-        # client side as well
-        if self._tracer is None:
-            from ddtrace.trace import tracer
-
-            self._tracer = tracer
-
-        span = self._tracer.current_span()
-        trace_id, span_id = (
-            (self._get_first_64_bits_of(span.trace_id), span.span_id)
-            if span
-            else (None, None)
-        )
-        return trace_id, span_id
-
     def format(self, record) -> str:
         """Convert logs to JSON."""
         log_time = datetime.fromtimestamp(record.created, timezone.utc).isoformat()
-        trace_id, span_id = self.get_trace_id_span_id()
         return json.dumps(
             {
                 "EventCount": 1,
@@ -173,16 +111,6 @@ class ASIMFormatter(logging.Formatter):
                 "EventType": record.name,
                 "EventSeverity": self.event_severity[record.levelname],
                 "EventOriginalSeverity": record.levelname,
-                "dd.application": "matchbox",
-                "dd.container_id": self.container_id,
-                "dd.env": self.env,
-                "dd.service": self.service,
-                "dd.source": "python",
-                "dd.sourcecategory": "sourcecode",
-                "dd.span_id": span_id,
-                "dd.team": "matchbox",
-                "dd.trace_id": trace_id,
-                "dd.version": self.version,
                 "message": record.getMessage(),
             },
         )

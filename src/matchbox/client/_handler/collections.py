2adf821294a0a2feb749da23d1532ef6ce663477
Will Langdale <will.langdale@businessandtrade.gov.uk>
2025-12-18 15:41:02 +0000

    All collections are public r/w for now.
    Signed-off-by: DBT pre-commit check

--- src/matchbox/client/_handler/collections.py
+++ src/matchbox/client/_handler/collections.py
@@ -16,6 +16,8 @@ from matchbox.common.dtos import (
     Collection,
     CollectionName,
     ModelResolutionPath,
+    PermissionGrant,
+    PermissionType,
     Resolution,
     ResolutionPath,
     ResourceOperationStatus,
@@ -57,8 +59,15 @@ def create_collection(name: CollectionName) -> ResourceOperationStatus:
     log_prefix = f"Collection {name}"
     logger.debug("Creating", prefix=log_prefix)
 
+    # All collections are public r/w for now
+    permission_grant = PermissionGrant(
+        group_name="public",
+        permission=PermissionType.WRITE,
+    )
+
     res = CLIENT.post(
         f"/collections/{name}",
+        json=[permission_grant.model_dump()],
     )
 
     return ResourceOperationStatus.model_validate(res.json())

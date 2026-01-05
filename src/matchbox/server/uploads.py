28141842b3abab0b166290703a9439107d3066d5
Sergio Recio Rodriguez <sergio.reciorodriguez@digital.trade.gov.uk>
2026-01-05 11:50:18 +0000

    fix: configuration prefetch argument
    Signed-off-by: DBT pre-commit check

--- src/matchbox/server/uploads.py
+++ src/matchbox/server/uploads.py
@@ -162,7 +162,7 @@ celery.conf.update(
     task_acks_late=True,
     # Reduce pre-fetching (workers reserving tasks while they're still busy)
     # as it's not ideal for long-running tasks
-    prefetch_multiplier=1,
+    worker_prefetch_multiplier=1,
     # Limit number of tasks a worker can complete before restarting
     # This may prevent OOM errors caused by memory not being deallocated properly
     worker_max_tasks_per_child=1,
